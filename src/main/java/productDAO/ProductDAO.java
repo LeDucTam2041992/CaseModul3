642a271479a4a29263050f7579a835431097b1e7
HaMinhKhue <khuesilicat@gmail.com>
2020-09-04 15:21:02 +0700

    jdbc add cj

--- src/main/java/productDAO/ProductDAO.java
+++ src/main/java/productDAO/ProductDAO.java
@@ -27,7 +27,7 @@ public class ProductDAO implements IProductDAO{
     protected Connection getConnection() {
         Connection connection = null;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
         } catch (SQLException e) {
             // TODO Auto-generated catch block
